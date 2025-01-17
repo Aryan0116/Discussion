0d4e41775092d46e388c50a61b60bb4ed41499ea
Aryan Singh <108578411+Aryan0116@users.noreply.github.com>
2025-01-18 01:51:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "*",
+        origin: "https://discussion-p6i2.onrender.com",
         methods: ["GET", "POST"]
     }
 });
